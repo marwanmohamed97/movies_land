9d5a232bc776527c2a2c1a9fdac0c93e1b418040
Marwan Mohammed <maranalhmde97@gmail.com>
2023-02-24 19:29:30 +0000

    add cusrom bottom navigation bar done

---
+++ lib/features/tv/data/presentaion/views/tv_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:movies_land/features/tv/data/presentaion/views/widgets/tv_view_body.dart';
+
+class TvView extends StatelessWidget {
+  const TvView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const TvViewBody();
+  }
+}
