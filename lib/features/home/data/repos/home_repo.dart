1afe2aa7ae3a02530b59069f920965158cab6e8f
Marwan Mohammed <maranalhmde97@gmail.com>
2023-02-25 19:19:57 +0000

    implement fetch trending movies method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,5 +3,5 @@ import 'package:movies_land/core/errors/failures.dart';
 import 'package:movies_land/features/home/data/models/movies_land_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, MoviesLandModel>> fetchTrendingMovies();
+  Future<Either<Failure, List<MoviesLandModel>>> fetchTrendingMovies();
 }
