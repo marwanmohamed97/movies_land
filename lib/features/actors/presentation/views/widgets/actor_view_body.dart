462b4b7ed6b292e09e4e8be5a90c8f36fb6b2863
Marwan Mohammed <maranalhmde97@gmail.com>
2023-02-27 15:57:33 +0000

    add actors cubit and similar movies cubit done

---
+++ lib/features/actors/presentation/views/widgets/actor_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ActorViewBody extends StatelessWidget {
+  const ActorViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
