bf1c3b08e2addee7706229c851693d67ae931b13
Marwan Mohammed <maranalhmde97@gmail.com>
2023-03-06 22:49:46 +0000

    add movies to favorites in farebase done

---
+++ lib/features/profile/presentation/views/widgets/favorites_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_land/constats.dart';
+import 'package:movies_land/features/home/presentation/manager/movies_details_cubit/movies_details_cubit.dart';
+
+class FavoritesViewBody extends StatelessWidget {
+  const FavoritesViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
